d01e6bd6b3f4398549d77bca841e0dcd9dd08183
Timothy J Fontaine <tjfontaine@gmail.com>
2022-12-04 13:27:26 -0800

    initial swift pm

---
+++ Tests/GPhotosTests/GPhotosTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import GPhotos
+
+final class GPhotosTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(GPhotos().text, "Hello, World!")
+    }
+}
